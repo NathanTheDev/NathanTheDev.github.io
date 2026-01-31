91169ad823e01336feaef345d772ab6de3cf5a02
Nathan <nathan.smith1922@gmail.com>
2026-01-31 20:43:41 +1100

    fix: updated commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), tanstackRouter()],
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
+
+  server: {
+    historyApiFallback: true,
+  },
 });
