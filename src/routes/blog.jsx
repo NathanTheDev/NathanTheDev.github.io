93e8ad3677c8e1ca457b1f4344a58c65a413b82a
Nathan Smith <NathanTheDev@gmail.com>
2026-02-09 16:05:26 +1100

    chore: added description for blog page

--- src/routes/blog.jsx
+++ src/routes/blog.jsx
@@ -18,6 +18,10 @@ function RouteComponent() {
         <div className="min-h-screen">
             <Navbar />
 
+            <p className="mt-10 mx-10 md:mx-50 lg:mx-100 p-5 justify-end bg-black/40 backdrop-blur-md text-xl text-center font-bold">Currently beginning to slowly progress through LeetCode Quest as
+                a bit of a side distraction and to recover some data structures,
+                and am documenting that here!</p>
+
             <div className="relative z-10 px-60 py-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {blogs.map(blog => (
                     <BlogCard key={blog.id} title={blog.title} content={blog.content} id={blog.id}/>
